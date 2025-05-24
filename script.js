aba7d070533aea71c0905ef4fea7cb1b022aeaf6
AndreiTheCoddingLearner <aboca755@gmail.com>
2025-05-24 21:01:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function openArticles() {
+    window.location.href="pagina.html";
+  }
+  
+  function openContact() {
+    window.location.href="contact.html";
+  }
+  
+  function openReviews() {
+    window.location.href="review.html";
+  }
+  (function(){
+    emailjs.init({
+      publicKey: "YRVxHpJW3iLR10r_B",
+    });
+ })();
